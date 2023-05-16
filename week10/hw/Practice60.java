ace335487056d4765fcb259b85373f7346d3c57c
ryu-hyesu <83686474+ryu-hyesu@users.noreply.github.com>
2023-05-16 23:52:08 +0900

    hw10

---
+++ week10/hw/Practice60.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+class Practice59 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String computer = getRandomRPS();
+        String user = null;
+
+        while (user == null) {
+            try {
+                user = getInput(scanner);
+            } catch (InvalidInputException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        int result = whoswin(computer, user);
+
+        System.out.println("컴퓨터의 생성: " + computer);
+        System.out.println("당신의 입력: " + user);
+
+        if (result == -1) {
+            System.out.println("컴퓨터가 이겼습니다.");
+        } else if (result == 1) {
+            System.out.println("당신이 이겼습니다.");
+        } else {
+            System.out.println("비겼습니다.");
+        }
+    }
+
+    public static int whoswin(String com, String you) {
+        if (com.equals("가위") && you.equals("바위") ||
+            com.equals("바위") && you.equals("보") ||
+            com.equals("보") && you.equals("가위")) {
+            return 1; // 사용자 승리
+        } else if (com.equals(you)) {
+            return 0; // 비김
+        } else {
+            return -1; // 컴퓨터 승리
+        }
+    }
+
+    public static String getRandomRPS() {
+        String[] rps = {"가위", "바위", "보"};
+        int index = (int) (Math.random() * rps.length);
+        return rps[index];
+    }
+
+    public static String getInput(Scanner scanner) throws InvalidInputException {
+        System.out.print("당신의 입력: ");
+        String input = scanner.next();
+
+        if (!input.equals("가위") && !input.equals("바위") && !input.equals("보")) {
+            throw new InvalidInputException("잘못된 입력입니다.");
+        }
+
+        return input;
+    }
+}
+
+class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
---
+++ week10/hw/Practice62.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+
+class NegativeBalanceException extends Exception {
+    public NegativeBalanceException(String message) {
+        super(message);
+    }
+}
+
+class BankAccount {
+    private String owner; // 계좌주 이름
+    private int balance; // 은행잔고
+
+    public BankAccount() {
+        this.owner = "모름";
+        this.balance = -1;
+    }
+
+    public BankAccount(String owner, int balance) {
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(int amount) throws NegativeBalanceException {
+        if (amount > balance) {
+            throw new NegativeBalanceException("출금액이 잔고보다 많습니다.");
+        }
+        balance -= amount;
+    }
+
+    @Override
+    public String toString() {
+        return owner + " " + balance;
+    }
+
+    public static void Practice62(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BankAccount[] accounts = new BankAccount[3];
+
+        for (int i = 0; i < accounts.length; i++) {
+            System.out.print("예금주 이름을 입력하세요: ");
+            String owner = scanner.nextLine();
+            System.out.print("잔고를 입력하세요: ");
+            int balance = scanner.nextInt();
+            scanner.nextLine();
+
+            accounts[i] = new BankAccount(owner, balance);
+        }
+
+        System.out.println("현재 잔고현황입니다.");
+        System.out.println("계좌번호 계좌주 잔고");
+        for (int i = 0; i < accounts.length; i++) {
+            System.out.println((i + 1) + " " + accounts[i].toString());
+        }
+
+        while (true) {
+            System.out.print("업데이트할 계좌번호를 입력하세요 (종료-0): ");
+            int accountNumber = scanner.nextInt();
+            scanner.nextLine();
+
+            if (accountNumber == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+
+            System.out.print("원하는 작업은 선택하세요 (1-입금/2-출금): ");
+            int operation = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.print("금액을 입력하세요: ");
+            int amount = scanner.nextInt();
+            scanner.nextLine();
+
+            BankAccount account = accounts[accountNumber - 1];
+
+            if (operation == 1) {
+                account.deposit(amount);
+                System.out.println("입금이 완료되었습니다.");
+            } else if (operation == 2) {
+                account.withdraw(amount);
+                System.out.println("출금이 완료되었습니다.");
+            }
+
+            System.out.println("현재 잔고현황입니다.");
+            System.out.println("계좌번호 계좌주 잔고");
+            for (int i = 0; i < accounts.length; i++) {
+                System.out.println((i + 1) + " " + accounts[i].toString());
+            }
+        }
+
+        scanner.close();
+    }
+}
